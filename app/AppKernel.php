f08b6e5ff9541146438cbdabdf188060b30a9c27
Damien Carcel <damien@carcel.net>
2014-11-11 19:15:40 +0100

    Use KNPMenuBundle for menu.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new Khatovar\WebBundle\KhatovarWebBundle(),
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
