bde336ee09d770b29354e911dbf2b1fd36e3e28d
Damien Carcel <damien@carcel.net>
2014-11-13 15:11:10 +0100

    Add xsolve-pl/xsolve-cookie-acknowledgement-bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Xsolve\CookieAcknowledgementBundle\XsolveCookieAcknowledgementBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
